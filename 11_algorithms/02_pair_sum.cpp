38a20237b8e192f03a955562277eb3f5eb5a573f
Malay Joshi <malayjoshi45@gmail.com>
2025-07-01 19:30:12 +0530

    Initial commit

---
+++ 11_algorithms/02_pair_sum.cpp
@@ -0,0 +1,51 @@
+// the optimal solution
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> PairSum(vector<int> nums, int target)
+{
+    vector<int> ans;
+    int n = nums.size();
+    int i = 0, j = n - 1;
+    while (i < j)
+    {
+        int pairSum = nums[i] + nums[j];
+        if (pairSum > target)
+        {
+            j--;
+        }
+        else if (pairSum < target)
+        {
+            i++;
+        }
+        else
+        {
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cout << "Enter the size of vector : ";
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter the element: ";
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> nums[i];
+    }
+    int target;
+    cout << "Enter target sum: ";
+    cin >> target;
+
+    vector<int> ans = PairSum(nums, target);
+    cout << ans[0] << " , " << ans[1] << endl;
+
+    return 0;
+}
